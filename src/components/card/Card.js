2756829b074bcd9e0df5cc5970d359ee86717574
minhaz <minhaz.taher@gmail.com>
2020-03-13 17:16:21 +0600

    height adjust

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -31,11 +31,13 @@ const Card = props => {
     <div className="card-comp">
       <div className="card">
         <img src={picture.large} alt="" />
-        <h4>{name}</h4>
-        <h4>{email}</h4>
-        <h4>Age : {dob.age}</h4>
-        <h4>Gender : {gender}</h4>
-        <h4>Phone : {phone}</h4>
+        <div className="pinfo">
+          <h4>{name}</h4>
+          <h4>{email}</h4>
+          <h4>Age : {dob.age}</h4>
+          <h4>Gender : {gender}</h4>
+          <h4>Phone : {phone}</h4>
+        </div>
         {Button}
       </div>
     </div>
