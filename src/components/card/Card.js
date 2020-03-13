fa2679f22f140c57249a07cbfcba7dff55f878af
minhaz <minhaz.taher@gmail.com>
2020-03-13 14:33:10 +0600

    btn disable

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -8,6 +8,14 @@ import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 const Card = props => {
   const { gender, name, email, dob, phone, picture } = props.friend;
 
+  let [btn, setBtn] = useState(false);
+  function btnn() {
+    if (btn == false) {
+      let newbtn = true;
+      setBtn(newbtn);
+    }
+  }
+
   return (
     <div className="card-comp">
       <div className="card">
@@ -17,7 +25,13 @@ const Card = props => {
         <h4>Age : {dob.age}</h4>
         <h4>Gender : {gender}</h4>
         <h4>Phone : {phone}</h4>
-        <button onClick={() => props.handleFriensList(props.friend)}>
+        <button
+          disabled={btn}
+          onClick={() => {
+            props.handleFriensList(props.friend);
+            btnn();
+          }}
+        >
           <FontAwesomeIcon icon={faUserPlus} />
           &nbsp; Be Friend!
         </button>
