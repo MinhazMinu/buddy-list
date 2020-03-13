12dbd65e062ae5cedb82251d4f7a4df575800e60
minhaz <minhaz.taher@gmail.com>
2020-03-13 17:09:36 +0600

    butn disable fixed

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
 import "./Card.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faUserPlus, faUserFriends } from "@fortawesome/free-solid-svg-icons";
 // ======================
 
 // ====================
 const Card = props => {
   const { gender, name, email, dob, phone, picture } = props.friend;
 
-  let [btn, setBtn] = useState(false);
-  function btnn() {
-    if (btn == false) {
-      let newbtn = true;
-      setBtn(newbtn);
-    }
+  const [isfriend, setIsfriend] = useState(false);
+  let Button = (
+    <button
+      onClick={() => {
+        props.handleFriensList(props.friend);
+        setIsfriend(true);
+      }}
+    >
+      <FontAwesomeIcon icon={faUserPlus} /> Be Friend!
+    </button>
+  );
+  if (isfriend) {
+    Button = (
+      <button className="disablebtn" disabled>
+        <FontAwesomeIcon icon={faUserFriends} /> Buddy!
+      </button>
+    );
   }
 
   return (
@@ -25,16 +36,7 @@ const Card = props => {
         <h4>Age : {dob.age}</h4>
         <h4>Gender : {gender}</h4>
         <h4>Phone : {phone}</h4>
-        <button
-          disabled={btn}
-          onClick={() => {
-            props.handleFriensList(props.friend);
-            btnn();
-          }}
-        >
-          <FontAwesomeIcon icon={faUserPlus} />
-          &nbsp; Be Friend!
-        </button>
+        {Button}
       </div>
     </div>
   );
